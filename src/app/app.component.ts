56630cd54c5603265ef21a3966a7d15d23523990
Abdulfattah Hessein <abdulfattah.hessein@gmail.com>
2024-10-04 15:28:38 +0300

    update app component to contain the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FriendRequest } from './models/friend-request.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-social-media';
+  friendRequests: FriendRequest[] = [
+    {
+      username: 'Ernest Achiever',
+      userPhoto: './assets/images/profile-5.jpg',
+      mutualFriendsCount: 5,
+      isAccepted: false,
+      date: new Date(),
+    },
+    {
+      username: 'Ernest Achiever',
+      userPhoto: './assets/images/profile-6.jpg',
+      mutualFriendsCount: 3,
+      isAccepted: false,
+      date: new Date(),
+    },
+    {
+      username: 'Ernest Achiever',
+      userPhoto: './assets/images/profile-7.jpg',
+      mutualFriendsCount: 8,
+      isAccepted: false,
+      date: new Date(),
+    },
+  ];
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -222,29 +222,6 @@ export class HomeComponent implements OnInit {
     },
   ];
 
-  friendRequests: FriendRequest[] = [
-    {
-      username: 'Ernest Achiever',
-      userPhoto: './assets/images/profile-5.jpg',
-      mutualFriendsCount: 5,
-      isAccepted: false,
-      date: new Date(),
-    },
-    {
-      username: 'Ernest Achiever',
-      userPhoto: './assets/images/profile-6.jpg',
-      mutualFriendsCount: 3,
-      isAccepted: false,
-      date: new Date(),
-    },
-    {
-      username: 'Ernest Achiever',
-      userPhoto: './assets/images/profile-7.jpg',
-      mutualFriendsCount: 8,
-      isAccepted: false,
-      date: new Date(),
-    },
-  ];
   constructor() {}
 
   ngOnInit() {}
